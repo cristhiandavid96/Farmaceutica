1ab0e343f8527087492150c78d7dbbf6c223a11f
David Velez <davidveleztorres96@gmail.com>
2021-08-08 01:28:07 -0500

    chart promociones

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
-import Vue from 'vue'
+import  {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
-import Echarts from 'vue-echarts'
-import 'echarts/lib/component/title'
-import 'echarts/lib/component/toolbox'
-import 'echarts/lib/component/tooltip'
-import 'echarts/lib/component/grid'
-import 'echarts/lib/component/legend'
-import 'echarts/lib/chart/line'
-import 'echarts/lib/chart/bar'
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
 
-Vue.component('chart',Echarts)
-new Vue({
-    render:h =>h(App)
-    
-}).use(router).use(store).mount('#app')
-
-
+Chartkick.use(Chart)
+createApp(App).use(router).use(store).mount('#app')
